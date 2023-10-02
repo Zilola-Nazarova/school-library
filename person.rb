8a8c4e6440edf363a48d405f097c5a9ec6994884
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-02 16:09:32 +0500

    Create TrimmerDecorator that inherits from Decorator and trims @nameable to max 10 characters

--- person.rb
+++ person.rb
@@ -46,3 +46,9 @@ class CapitalizeDecorator < Decorator
     @nameable.correct_name.capitalize
   end
 end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name[0...10]
+  end
+end
