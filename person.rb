b2a2eeb2ddfaf93176c0b7bacde5e39649a43f8e
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-02 16:30:18 +0500

    Add ternary operator to make sure that no mutations are done to the @nameable with 10 or less chars

--- person.rb
+++ person.rb
@@ -50,6 +50,6 @@ end
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name[0...10]
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0...10] : @nameable.correct_name
   end
 end
