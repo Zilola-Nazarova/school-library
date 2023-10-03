eaac6d3a9082a336f552e527837ea1e21dd78e4d
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-03 11:05:35 +0500

    Implement belongs-to relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
