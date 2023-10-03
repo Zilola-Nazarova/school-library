76cc668936d078c1dba70388a04698bd3c045228
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-03 20:50:13 +0500

    Write the options text

--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
 require './app'
 
+def show_options
+  puts "\nPlease choose and option by entering a number:
+1 - List all books\n2 - List all people\n3 - Create a person
+4 - Create a book\n5 - Create a rental
+6 - List all rentals for a given person id\n7 - Exit"
+end
+
 def main
   app = App.new
   app.run
