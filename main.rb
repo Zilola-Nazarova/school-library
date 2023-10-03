c81b531ad1bdbc4d377e2e29f43853564f859214
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-03 20:51:18 +0500

    Make calls to corresponding functions depending on the selected options

--- main.rb
+++ main.rb
@@ -17,6 +17,25 @@ def prompt
   end
 end
 
+def call_option(option)
+  case option
+  when 1
+    list_books
+  when 2
+    list_people
+  when 3
+    create_person
+  when 4
+    create_book
+  when 5
+    create_rental
+  when 6
+    list_rentals
+  else
+    puts "Choose a number between 1 and 7"
+  end
+end
+
 def main
   app = App.new
   app.run
