e68cf460f9699bf535d1742594cd92b4d7e37448
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-03 13:47:12 +0500

    Chenge @student instance variable to @person

--- rental.rb
+++ rental.rb
@@ -1,14 +1,14 @@
 class Rental
-  attr_reader :book, :student
+  attr_reader :book, :person
   attr_accessor :date
 
-  def initialize(date, book, student)
+  def initialize(date, book, person)
     @date = date
 
     @book = book
     book.rentals << self
 
-    @student = student
-    student.rentals << self
+    @person = person
+    person.rentals << self
   end
 end
