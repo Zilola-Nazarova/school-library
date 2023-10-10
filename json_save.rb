c5cee6d89490d5c4a6623ca9aee0c8c58e70e4f6
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-10-10 11:59:03 +0500

    Add logic to create 3 files and write data to it when user exits application

--- json_save.rb
+++ json_save.rb
@@ -3,5 +3,20 @@ require 'json'
 module Save
   def save_files
     puts "Saving files"
+    save_books
+    save_people
+    save_rentals
+  end
+
+  def save_books
+    File.open("./books.json", "w") {|f| f.write("Books list") }
+  end
+
+  def save_people
+    File.open("./people.json", "w") {|f| f.write("Pelple list") }
+  end
+
+  def save_rentals
+    File.open("./rentals.json", "w") {|f| f.write("Rentals list") }
   end
 end
